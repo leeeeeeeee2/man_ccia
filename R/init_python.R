1cd2aa90fc8cb261eff75cdf3c613fe578f807fc
Conor Anderson <conor@conr.ca>
2019-02-27 16:20:06 -0500

    Add hints to lab 4, label the chunks in lab 1, update the dockerfile.
    Squashed commit of the following:
    
    commit 8adb5777d173f2ae4ef7f109f8e143ec45db8792
    Author: Conor Anderson <conor@conr.ca>
    Date:   Wed Feb 27 16:19:47 2019 -0500
    
        Update dockerfile
    
    commit e9cf24d3ca69d1255b53e9e67b551b07b6f5a607
    Author: Conor Anderson <conor@conr.ca>
    Date:   Wed Feb 27 15:15:55 2019 -0500
    
        Name all lab 1 chunks to find stall point
    
    commit b6c824249fb9fa56d22cce325542778d898f5c72
    Author: Conor Anderson <conor@conr.ca>
    Date:   Wed Feb 27 13:21:11 2019 -0500
    
        Add lots of hints to lab 4.

--- R/init_python.R
+++ R/init_python.R
@@ -1,4 +1,7 @@
 library(reticulate)
 conda <- ifelse(file.exists("/opt/conda/bin/conda"), "/opt/conda/bin/conda", "/data/home-ext/miniconda3/bin/conda")
 use_condaenv("man_ccia", conda = conda, required = TRUE)
-if (interactive()) repl_python()
+if (interactive()) {
+  message("Running interactively. Starting REPL.")
+  repl_python()
+}
